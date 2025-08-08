3ae8fcc9e0c1aa81a10fcc8fc22a4bc265f9f3c9
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-08-08 16:34:33 +0300

    feat: add basic repository interface for crud ops

--- lib/features/product/domain/repositories/product_repository.dart
+++ lib/features/product/domain/repositories/product_repository.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/error/failure.dart';
 import '../entities/product.dart';
 
+// Define the ProductRepository repository interface, which may include methods for fetching, creating, updating, and deleting products from the data source through API calls, database queries, or other means.
+
 abstract class ProductRepository {
   Future<Either<Failure, List<Product>>> getProducts();
   Future<Either<Failure, Product>> getProduct(String id);
